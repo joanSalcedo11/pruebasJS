f92cfa76dd3250ab8dcd988fe71ecd8cec0e2eda
joanSalcedo11 <159442401+joanSalcedo11@users.noreply.github.com>
2025-05-01 22:56:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require("express");
-// 🔹 Servidor Express (siempre activo)
 const app = express();
 app.get("/", (req, res) => {
   res.send(
-    setInterval(function() {
-      for(var n=1;n=40;n++){
-      var mensaje = "hola mundo"+n;
-    }
-      return mensaje;
-    }, 15000); // 15000 milisegundos = 15 segundos
+   setInterval(function() {
+     for(var n=1;n=40;n++){
+       var mensaje = "hola mundo"+n;
+     }
+   }
+         return mensaje;
+   },15000);
   );
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor listo en puerto ${PORT}`);
 });
+
