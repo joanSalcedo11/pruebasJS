c6859e00c9aec87e1fa810fc9341545f8bdcb5ff
joanSalcedo11 <159442401+joanSalcedo11@users.noreply.github.com>
2025-05-01 23:16:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // 🔹 Servidor Express (siempre activo)
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,'intervalo.js'))
 });
