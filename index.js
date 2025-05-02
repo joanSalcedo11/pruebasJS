b585297c18828d4e8f17cae12a02fd18625dd7c8
joanSalcedo11 <159442401+joanSalcedo11@users.noreply.github.com>
2025-05-01 23:15:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor listo en puerto ${PORT}`);
-});
-
+// 🔹 Servidor Express (siempre activo)
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,'intervalo.js'))
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(🚀 Servidor listo en puerto ${PORT});
+});
+
