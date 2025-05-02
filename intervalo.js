abdd418f414487076644002c6edd391ff75eb384
joanSalcedo11 <159442401+joanSalcedo11@users.noreply.github.com>
2025-05-01 23:30:53 -0500

    Update intervalo.js

--- intervalo.js
+++ intervalo.js
@@ -1,5 +1,14 @@
- setInterval(function() {
-      for(var n=1;n=40;n++){
-      console.log('hola mundo' + n)
-      }
-    }, 15000); // 15000 milisegundos = 15 segundos
+let contador = 1;
+let mensajeActual = `Hola mundo ${contador}`;
+
+setInterval(() => {
+  contador++;
+  mensajeActual = `Hola mundo ${contador}`;
+  console.log(mensajeActual); // solo para ver en consola
+}, 15000); // cada 15 segundos
+
+function obtenerMensaje() {
+  return mensajeActual;
+}
+
+module.exports = obtenerMensaje;
