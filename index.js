06603bdb4c7f1005c96a7accd37f6d0246c8fc52
joanSalcedo11 <159442401+joanSalcedo11@users.noreply.github.com>
2025-05-01 23:20:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Servidor listo en puerto ${PORT});
+  console.log('Servidor listo en puerto ${PORT}');
 });
 
